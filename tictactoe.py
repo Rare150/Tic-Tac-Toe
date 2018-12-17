15a8976d4adbccce759c003187bc6a8975f377c9
Rare150 <45627899+Rare150@users.noreply.github.com>
2018-12-17 20:19:51 +0200

    TTT First commit

---
+++ tictactoe.py
@@ -0,0 +1,24 @@
+def drawNewChart():
+    print("     │     │     ")
+    print("     │     │     ")
+    print("     │     │     ")
+    print("──────╞──────╞──────")
+    print("     │     │     ")
+    print("     │     │     ")
+    print("     │     │     ")
+    print("──────╞──────╞──────")
+    print("     │     │     ")
+    print("     │     │     ")
+    print("     │     │     ")
+
+def drawCircle():
+    print(" /¯\ ")
+    print("|   |")
+    print(" \_/ ")
+
+def drawX():
+    print(" \ / ")
+    print("  X  ")
+    print(" / \ ")
+
+print("")
